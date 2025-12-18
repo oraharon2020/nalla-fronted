60fb27d3ec01ff0d06f53a1d02b4dbc26513350a
Or Aharon <or.aharon1122@gmail.com>
2025-12-18 23:05:27 +0200

    Fix: use product.image instead of product.images

--- src/app/product/[slug]/ProductPageClient.tsx
+++ src/app/product/[slug]/ProductPageClient.tsx
@@ -512,7 +512,7 @@ export function ProductPageClient({ product, variations = [], faqs = [], video =
             <AdminProductFields
               basePrice={parseFloat(product.price?.replace(/[^\d.]/g, '') || '0')}
               variationPrice={selectedVariation ? parseFloat(selectedVariation.price || '0') : undefined}
-              productImage={product.images?.[0]?.src}
+              productImage={product.image?.src || product.galleryImages?.[0]?.src}
               productName={product.name}
               onPriceChange={(newPrice, data) => {
                 setAdminPrice(newPrice);
